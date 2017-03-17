92c7c3430c49bdd104a33dedaeb18b4e45d87fda
simsss <simone.ferretti@roma2.infn.it>
2017-03-17 14:49:23 +0000

    update cron script

--- marketplace/moodle/scripts/script.sh
+++ marketplace/moodle/scripts/script.sh
@@ -2,11 +2,10 @@
 
 sed -i  -e "s/\$DB_HOST/$DB_HOST/g" -e "s/\$DB_NAME/$DB_NAME/g" -e "s/\$DB_USER/$DB_USER/g" -e "s/\$DB_PASSWD/$DB_PASSWD/g" -e "s|\$WWW_ROOT|$WWW_ROOT|g" /var/www/html/config.php
 
-
-crontab -u www-data /scripts/cronphp
-
-service apache2 restart
-
 service cron start
+
+crontab -u www-data /root/scripts/cronphp
+
+service apache2 start
 
 tail -f /var/log/apache2/error.log
