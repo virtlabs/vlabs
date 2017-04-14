2920b961b8bd9d0b0664d48517f68131125ef013
Federico Zani <federico.zani@roma2.infn.it>
2017-04-14 10:42:20 +0200

    Set password for notebook

--- marketplace/jupyter/root/notebook.sh
+++ marketplace/jupyter/root/notebook.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Prepare ROOT env
 . /RT/root/bin/thisroot.sh
 
+# Set notebook password
+./setpasswd.py
+
+# Launch notebook
 /opt/conda/bin/jupyter notebook --notebook-dir=/opt/notebooks --ip='*' --port=8888 --no-browser
